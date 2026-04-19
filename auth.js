b79d6a47e52b6cab4c1f7ec0cc76809ef60b2889
prachi1665 <prachiatul2009@gmail.com>
2026-04-19 14:37:46 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,21 @@
+function login() {
+    let user = document.getElementById("username").value;
+    let pass = document.getElementById("password").value;
 
+    if (user === "" || pass === "") {
+        document.getElementById("msg").innerText = "Fill all fields!";
+        return;
+    }
+
+    // fake login (frontend only)
+    localStorage.setItem("user", user);
+
+    window.location.href = "dashboard.html";
+}
+
+// auto redirect if already logged in
+window.onload = function () {
+    if (localStorage.getItem("user")) {
+        window.location.href = "dashboard.html";
+    }
+};
